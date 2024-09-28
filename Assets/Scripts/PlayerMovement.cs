67c4ee49290d08a5b52fb37d01d7a8913831caad
the gamer <133170486+a01username@users.noreply.github.com>
2024-09-28 12:03:23 -0700

    Assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,10 +11,26 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        Move();
     }
 
-    void Move(){
-        float x = Input.GetAxis("Horizontal");
+    void Move()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector2(transform.position.x+1, transform.position.y);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector2(transform.position.x-1, transform.position.y);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y+1);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y-1);
+        }
     }
 }
