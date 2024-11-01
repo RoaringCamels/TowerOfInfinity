e1e0497774a006f2bff5fc03b5aa585cf8999e66
qstokes <quincy.f.stokes@gmail.com>
2024-11-01 16:50:33 -0700

    Enemy and SubtractHitBox

---
+++ Assets/Scripts/SubtractHitbox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubtractHitbox : MonoBehaviour
+{
+    PlayerAttack playerAttack;
+
+    void Start() {
+        playerAttack = GetComponent<PlayerAttack>();
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+                          
+        if(other.CompareTag("Enemy")) {
+            //deal whatever amount of damage
+            other.gameObject.GetComponent<Enemy>().SubtractDamage(56.9f);
+        }
+        //instead of just destroying, perform the corresponding sword attack damage
+    }
+
+}
