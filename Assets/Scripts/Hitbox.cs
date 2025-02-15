81147ae53026865b8faf043a76111959f7c6bdd4
qstokes <quincy.f.stokes@gmail.com>
2025-02-15 13:03:33 -0800

    boss health reduction

--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -12,12 +12,25 @@ public class Hitbox : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {    
         if(other.CompareTag("Enemy")) {
             //deal whatever amount of damage
-            other.gameObject.GetComponentInParent<Enemy>().ChangeHealth(playerAttack.performOperation());
+            Enemy enemy = other.gameObject.GetComponentInParent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.ChangeHealth(playerAttack.performOperation());
+                return;
+            }
+            Bosslev1 boss = other.gameObject.GetComponentInParent<Bosslev1>();
+            if(boss != null)
+            {
+                boss.ChangeHealth(playerAttack.performOperation());
+                return;
+            }
+
         }
         if(other.CompareTag("Projectile")) {
 
             other.gameObject.GetComponent<Projectile>().ChangeDamage(playerAttack.performOperation());
         }
+        
         //instead of just destroying, perform the corresponding sword attack damage
     }
 
